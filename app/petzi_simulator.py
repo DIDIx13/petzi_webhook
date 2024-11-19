be344e628ea321d13ba6473ade16bfc05640dbf9
Darwin Ameli <darwin.ameli@rpn.ch>
2024-11-19 04:06:33 +0100

    revert to original

--- app/petzi_simulator.py
+++ app/petzi_simulator.py
@@ -12,41 +12,46 @@ import requests
 import random
 import string
 import json
-import os
-from dotenv import load_dotenv
 
 def generate_random_string(length=12):
     letters_and_digits = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters_and_digits) for _ in range(length))
+    return ''.join(random.choice(letters_and_digits) for i in range(length))
+
 
 def make_header(body, secret):
-    unix_timestamp = str(int(datetime.datetime.timestamp(datetime.datetime.now())))
+    unix_timestamp = str(datetime.datetime.timestamp(datetime.datetime.now())).split('.')[0]
     body_to_sign = f'{unix_timestamp}.{body}'.encode()
     digest = hmac.new(secret.encode(), body_to_sign, "sha256").hexdigest()
-    headers = {
-        'Petzi-Signature': f't={unix_timestamp},v1={digest}',
-        'Petzi-Version': '2',
-        'Content-Type': 'application/json',
-        'User-Agent': 'PETZI webhook'
-    }
+    # Set the headers for the POST request
+    headers = {'Petzi-Signature': f't={unix_timestamp},v1={digest}', 'Petzi-Version': '2',
+               'Content-Type': 'application/json', 'User-Agent': 'PETZI webhook'}
     return headers
 
+
 def make_post_request(url, data, secret):
-    session = requests.Session()
     try:
-        headers = make_header(data, secret)
-        response = session.post(url, data=data.encode('utf-8'), headers=headers)
+        # Make the POST request
+        response = requests.post(url, data=data.encode('utf-8'), headers=make_header(data, secret))
+
         if response.status_code == 200:
             print(f"Request successful. Response: {response.text}")
         else:
-            print(f"Request failed with status code {response.status_code}. Response: {response.text}")
+            print(f"Request failed with status code {response.status_code}.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    finally:
-        session.close()
 
-def simulate_webhook(url, secret):
-    # Simulated JSON data
+
+if __name__ == "__main__":
+    # Create a command line argument parser
+    parser = argparse.ArgumentParser(description="HTTP POST Request with JSON Body")
+    parser.add_argument("url", type=str, help="URL to send the POST request to")
+    parser.add_argument("secret", nargs='?', type=str, help="secret shared between your server and petzi simulator",
+                        default="AEeyJhbGciOiJIUzUxMiIsImlzcyI6")
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Coding horror : Don't Do This at Home!
     data = '''
     {
            "event":"ticket_created",
@@ -92,6 +97,5 @@ def simulate_webhook(url, secret):
     data_dict = json.loads(data)
     data_dict["details"]["ticket"]["number"] = generate_random_string()
     data = json.dumps(data_dict, indent=4)
-
     # Make the POST request
-    make_post_request(url, data, secret)
+    make_post_request(args.url, data, args.secret)
